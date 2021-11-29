076b065f8906e2450ce8f261024b0c4dfece87d1
EC2 Default User <ec2-user@ip-172-31-87-32.ec2.internal>
2021-11-29 07:59:08 +0000

    login page fast commit

--- app/controllers/zanges_controller.rb
+++ app/controllers/zanges_controller.rb
@@ -2,4 +2,8 @@ class ZangesController < ApplicationController
   def index
     @zanges = Zange.all
   end
+  
+  def new
+    @zange = Zange.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'zanges', to: 'zanges#index'
-  root 'zanges#index'
+  get "application/new"
+  root "application#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
